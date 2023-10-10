2f4d75aba186c2e58e26e3a7d765f169b685c9e9
ShaneByrne0963 <>
2023-10-10 10:46:39 +0000

    Add template for tasks page

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+// Setting up the side navbar for mobile devices
+document.addEventListener('DOMContentLoaded', function () {
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
