3ecb67628e27e6431b1e3b8d82af083d1c56590d
ShaneByrne0963 <>
2023-10-11 11:21:30 +0000

    Add ability to create and read tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,26 @@
-// Setting up the side navbar for mobile devices
 document.addEventListener('DOMContentLoaded', function () {
+    // Setting up the side navbar for mobile devices
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-});
 
-// Modals for verification to delete categories
-document.addEventListener('DOMContentLoaded', function () {
+    // Modals for verification to delete categories
     var modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
+
+    // Date Picker
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm yyyy",
+        i18n: {
+            done: 'Select'
+        }
+    });
+
+    // Select Dropdown
+    var selectDropdown = document.querySelectorAll('select');
+    M.FormSelect.init(selectDropdown);
+
+    // Collapsibles
+    var collapse = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapse);
 });
